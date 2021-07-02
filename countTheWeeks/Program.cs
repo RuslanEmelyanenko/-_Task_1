db3ee132916f0ff19527dac1ac3e53b2013b13dd
RuslanEmelyanenko <Ruslan.Emelyanenka@gmail.com>
2021-07-02 14:25:00 +0300

    C#_Task_1

---
+++ countTheWeeks/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace countTheWeeks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Таск 1.0
+            // Подсчитать колличество полных недель!
+
+            short days = 234;
+            const byte week = 7;
+
+            Console.WriteLine($"In 234 days is: {days / week} weeks!");
+        }
+    }
+}
